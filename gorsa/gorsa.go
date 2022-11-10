253c4bed381613c7f0556092329233d998e41f7a
duquancai <55043508+duquancai@users.noreply.github.com>
2022-11-10 09:40:59 +0800

    Add files via upload

--- gorsa/gorsa.go
+++ gorsa/gorsa.go
@@ -6,7 +6,6 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"math/big"
-	"strings"
 )
 
 // 公钥加密
@@ -70,20 +69,20 @@ func PriKeyDecrypt(data []byte, priPEM string) ([]byte, error) {
 }
 
 // RSANoPadding,modulus为16进制字符串,仅适合0-255的字符加密
-func PubKeyEncrypt_NoPad(msg, hexE, hexN, reserve string) []byte {
-	hexRKey := ""
-	// reserve逆序msg字符串  仅适合0-255的字符
-	switch strings.ToUpper(reserve) {
-	case "ASC":
-		bytes := []rune(msg)
+func PubKeyEncrypt_NoPad(msg, hexE, hexN string, args ...interface{}) []byte {
+	c := new(big.Int)
+
+	switch args {
+	case nil:
+		//逆序msg字符串  仅适合0-255的字符
+		bytes := []byte(msg)
 		for from, to := 0, len(bytes)-1; from < to; from, to = from+1, to-1 {
 			bytes[from], bytes[to] = bytes[to], bytes[from]
 		}
-		hexRKey = string(bytes)
+		c.SetBytes(bytes)
 	default:
-		hexRKey = msg
+		c.SetBytes([]byte(msg))
 	}
-	c := new(big.Int).SetBytes([]byte(hexRKey))
 
 	bigN := new(big.Int)
 	bigN.SetString(hexN, 16)
@@ -103,7 +102,18 @@ func PriKeyDecrypt_NoPad(msg []byte, priPEM string) string {
 }
 
 func PubKeyEncrypt_E_N(msg, hexE, hexN string) []byte {
-	pubkey, _ := ReadNE(hexN, hexE)
+	// pubkey, _ := ReadNE(hexN, hexE)
+	bigE := new(big.Int)
+	bigE.SetString(hexE, 16)
+	// fmt.Println(int(bigE.Int64()))
+	bigN := new(big.Int)
+	bigN.SetString(hexN, 16)
+	// temp, _ := base64.RawStdEncoding.DecodeString(hexN)
+	// bigN.SetBytes(temp)
+	pubkey := &rsa.PublicKey{
+		E: int(bigE.Int64()),
+		N: bigN,
+	}
 	resb, _ := rsa.EncryptPKCS1v15(rand.Reader, pubkey, []byte(msg))
 	return resb
 }
@@ -118,6 +128,12 @@ func PubKeyEncrypt_b64(plainText []byte, pubBase64 string) ([]byte, error) {
 	rsaPk, err := x509.ParsePKIXPublicKey(publicKeyBinary)
 	if err != nil {
 		return nil, err
+	}
+	if err != nil {
+		rsaPk, err = x509.ParsePKCS1PublicKey(publicKeyBinary)
+		if err != nil {
+			return nil, err
+		}
 	}
 	var pubKey *rsa.PublicKey = rsaPk.(*rsa.PublicKey)
 	cipherText, err := rsa.EncryptPKCS1v15(rand.Reader, pubKey, plainText)
