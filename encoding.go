9626d4e6e4c114b257160e898a92f33defd509a7
duquancai <55043508+duquancai@users.noreply.github.com>
2022-11-10 09:35:42 +0800

    Add files via upload

--- encoding.go
+++ encoding.go
@@ -3,6 +3,10 @@ package encryptlib
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"net/url"
+	"strings"
+
+	"github.com/qiniu/iconv"
 )
 
 func Base64Encode(src []byte) string {
@@ -18,4 +22,19 @@ func HexEncode(src []byte) string {
 }
 func HexDecode(s string) ([]byte, error) {
 	return hex.DecodeString(s)
+}
+
+func EncodeURIComponent(str string) string {
+	r := url.QueryEscape(str)
+	r = strings.Replace(r, "+", "%20", -1)
+	return r
+}
+
+func EncodeConvert(tocode string, fromcode string, souse string) string {
+	cd, err := iconv.Open(tocode, fromcode) // convert utf-8 to gbk
+	if err != nil {
+		return ""
+	}
+	defer cd.Close()
+	return cd.ConvString(souse)
 }
